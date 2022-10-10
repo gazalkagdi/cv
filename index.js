8be604680f6a4d2e441b96cb7108694b2f35bb38
gazalkagdi <95044565+gazalkagdi@users.noreply.github.com>
2022-10-10 14:08:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+$('.slidee').css('animation','none');
+setTimeout(function(){ $('.slidee').css('animation','typing  1.5s steps(40, end),blink-caret .5s step-end infinite'); },2000)
+
+$('.slidee').css('visibility','hidden');
+setTimeout(function(){ $('.slidee').css('visibility','visible'); },2000)
+
+
+$('.slideUp').css('animation','none');
+setTimeout(function(){ $('.slideUp').css('animation','slideUp ease 1.5s forwards 1.2s'); },2000)
+
+$('.slideUp').css('visibility','hidden');
+setTimeout(function(){ $('.slideUp').css('visibility','visible'); },3500)
+
+
+$('.slideUp2').css('animation','none');
+setTimeout(function(){ $('.slideUp2').css('animation','slideUp ease 1.5s forwards 1.2s'); },2000)
+
+$('.slideUp2').css('visibility','hidden');
+setTimeout(function(){ $('.slideUp2').css('visibility','visible'); },3500)
